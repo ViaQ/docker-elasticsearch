a4e9997fc2b70fe0854c411ca0bc6a8a3a26fef6
Rich Megginson <rmeggins@redhat.com>
2015-11-04 13:50:34 -0700

    works with plain docker build/run

---
+++ run-container.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+HOSTDIR=${HOSTDIR:-/var/lib/bitscout}
+ESHOSTDIR=${ESHOSTDIR:-$HOSTDIR/elasticsearch}
+if [ ! -d $ESHOSTDIR ] ; then
+    sudo mkdir -p $ESHOSTDIR
+    sudo chown -R $USER $ESHOSTDIR
+    sudo chcon -Rt svirt_sandbox_file_t $ESHOSTDIR
+fi
+
+uid=`id -u`
+id=`docker run -d -p 9200:9200 -p 9300:9300 -u $uid --name bitscout-elasticsearch -v $ESHOSTDIR:/elasticsearch bitscout/elasticsearch:latest`
+echo $id
+sleep 5
+docker logs $id
