39717863434e1f9de4f491d1922eb437fbe3ffce
Rich Megginson <rmeggins@redhat.com>
2016-01-26 09:17:29 -0700

    rename bitscout to viaq

--- run-container.sh
+++ run-container.sh
@@ -4,10 +4,10 @@ set -o errexit
 DB_IN_CONTAINER=${DB_IN_CONTAINER:-0}
 
 if [ "$DB_IN_CONTAINER" = 1 ]; then
-    id=`docker run -d -p 9200:9200 -p 9300:9300 --name bitscout-elasticsearch bitscout/elasticsearch:latest`
+    id=`docker run -d -p 9200:9200 -p 9300:9300 --name viaq-elasticsearch viaq/elasticsearch:latest`
 else
     # requires an external mount point and specific external uid
-    HOSTDIR=${HOSTDIR:-/var/lib/bitscout}
+    HOSTDIR=${HOSTDIR:-/var/lib/viaq}
     ESHOSTDIR=${ESHOSTDIR:-$HOSTDIR/elasticsearch}
     if [ ! -d $ESHOSTDIR ] ; then
         sudo mkdir -p $ESHOSTDIR
@@ -15,7 +15,7 @@ else
         sudo chcon -Rt svirt_sandbox_file_t $ESHOSTDIR
     fi
     uid=`id -u`
-    id=`docker run -d -p 9200:9200 -p 9300:9300 -u $uid --name bitscout-elasticsearch -v $ESHOSTDIR:/elasticsearch bitscout/elasticsearch:latest`
+    id=`docker run -d -p 9200:9200 -p 9300:9300 -u $uid --name viaq-elasticsearch -v $ESHOSTDIR:/elasticsearch viaq/elasticsearch:latest`
 fi
 
 echo $id
